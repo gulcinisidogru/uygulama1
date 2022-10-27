cbc55637c1c0d760c72a664d20a57dee75d816ae
gulci <gulci@LAPTOP-KPNR185M>
2022-10-27 15:11:17 +0300

    Proje dosyası ekle.

---
+++ uygulama1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sonuc1;
+
+            sonuc1 = 48 / 6 / 4;
+
+            Console.WriteLine("İşlemin sonucu = {0} ", sonuc1);
+
+            Console.ReadKey();
+
+
+
+
+
+
+            int sonuc2;
+
+            sonuc2 = 24/ 3 *2;
+
+            Console.WriteLine("İşlemin sonucu = {0} ", sonuc2);
+
+            Console.ReadKey();
+
+
+
+
+            int sonuc3;
+
+            sonuc3 = 2*2 +5-1+4;
+
+            Console.WriteLine("İşlemin sonucu = {0} ", sonuc3);
+
+            Console.ReadKey();
+
+
+
+            int sonuc4;
+
+            sonuc4 = (7+4)*2-1+8/2;
+
+            Console.WriteLine("İşlemin sonucu = {0} ", sonuc4);
+
+            Console.ReadKey();
+
+
+
+
+            int sonuc5;
+
+            sonuc5 = (5-1)*2-1+7*7/2;
+
+            Console.WriteLine("İşlemin sonucu = {0} ", sonuc5);
+
+            Console.ReadKey();
+
+
+
+          
+
+
+
+
+
+
+        }
+    }
+}
